8aa0a39f1af0cd841d294f2747107403dbd25aff
Daniel Metz <danielmmetz@gmail.com>
2021-11-27 23:36:03 -0600

    go all-in on lua for neovim; adopt paq over vim-plug

--- nvim.go
+++ nvim.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,29 +12,8 @@ import (
 )
 
 type Nvim struct {
-	PluginDir string     `json:"plugin_dir"`
-	Plugins   []Plugin   `json:"plugins"`
-	Config    NvimConfig `json:"config"`
-}
-
-func (v *Nvim) UnmarshalJSON(b []byte) error {
-	type clone struct {
-		PluginDir string     `json:"plugin_dir"`
-		Plugins   []Plugin   `json:"plugins"`
-		Config    NvimConfig `json:"config"`
-	}
-	var c clone
-	if err := json.Unmarshal(b, &c); err != nil {
-		return err
-	}
-	expandedPluginDir, err := expandTilde(c.PluginDir)
-	if err != nil {
-		return err
-	}
-	v.PluginDir = expandedPluginDir
-	v.Plugins = c.Plugins
-	v.Config = c.Config
-	return nil
+	Plugins []Plugin   `json:"plugins"`
+	Config  NvimConfig `json:"config"`
 }
 
 func (v *Nvim) Ensure(ctx context.Context) error {
@@ -43,61 +21,15 @@ func (v *Nvim) Ensure(ctx context.Context) error {
 		return nil
 	}
 
-	if err := v.ensureVimPlug(ctx); err != nil {
-		return fmt.Errorf("error ensuring vim-plug: %w", err)
-	}
 	if err := v.ensureInitVim(); err != nil {
-		return fmt.Errorf("error ensuring init.vim: %w", err)
+		return fmt.Errorf("error ensuring init.lua: %w", err)
 	}
 	fmt.Println("installing neovim plugins")
-	installCmd := exec.CommandContext(ctx, "nvim", "--headless", "+PlugInstall", "+qa")
+	installCmd := exec.CommandContext(ctx, "nvim", "--headless", "+PaqSync", "+qa")
 	if output, err := installCmd.CombinedOutput(); err != nil {
-		return fmt.Errorf("error running `nvim --headless +PlugInstall +qa`: %w\n%s", err, string(output))
+		return fmt.Errorf("error running neovim plugin sync commands: %w\n%s", err, string(output))
 	}
 	return nil
-}
-
-const (
-	vimPlugURL = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-)
-
-func (v *Nvim) ensureVimPlug(ctx context.Context) error {
-	if len(v.Plugins) == 0 {
-		return nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return fmt.Errorf("unable to determine home dir: %w", err)
-	}
-	dstPath := filepath.Join(home, ".local", "share", "nvim", "site", "autoload", "plug.vim")
-
-	_, err = os.Stat(dstPath)
-	if err == nil {
-		fmt.Println("skipping vim-plug install: already present")
-		return nil // file already exists
-	} else if err != nil && !os.IsNotExist(err) {
-		return err
-	}
-
-	fmt.Println("installing vim-plug")
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, vimPlugURL, nil)
-	if err != nil {
-		return err
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	if err := os.MkdirAll(filepath.Dir(dstPath), 0755); err != nil {
-		return err
-	}
-	return ioutil.WriteFile(dstPath, content, 0755)
 }
 
 func (v *Nvim) ensureInitVim() error {
@@ -108,29 +40,32 @@ func (v *Nvim) ensureInitVim() error {
 	if err != nil {
 		return fmt.Errorf("unable to determine home dir: %w", err)
 	}
-	cfgPath := filepath.Join(home, ".config", "nvim", "init.vim")
+	cfgPath := filepath.Join(home, ".config", "nvim", "init.lua")
 	if err := os.MkdirAll(filepath.Dir(cfgPath), 0755); err != nil {
 		return fmt.Errorf("error making intermediate directories for %s: %w", cfgPath, err)
 	}
 	fmt.Println("writing vim config to", cfgPath)
-	return ioutil.WriteFile(cfgPath, []byte(v.initVim()), 0755)
+	return ioutil.WriteFile(cfgPath, []byte(v.initLua()), 0755)
 }
 
-func (v *Nvim) destDir(p Plugin) string {
-	return fmt.Sprintf("%s/%s", v.PluginDir, p.Repo)
-}
+const paqBootstrap = `-- boostrap paq
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+end
+`
 
-func (v *Nvim) initVim() string {
-	var vimPlugLines []string
-	vimPlugLines = append(vimPlugLines, fmt.Sprintf("call plug#begin('%s')", v.PluginDir))
+func (v *Nvim) initLua() string {
+	var pluginLines []string
+	pluginLines = append(pluginLines, paqBootstrap)
+	pluginLines = append(pluginLines, `require "paq" {`)
 	for _, plugin := range v.Plugins {
-		vimPlugLines = append(vimPlugLines, plugin.toVimPlug())
+		pluginLines = append(pluginLines, fmt.Sprintf(`  "%v";`, plugin))
 	}
-	vimPlugLines = append(vimPlugLines, "call plug#end()")
-	vimPlugLines = append(vimPlugLines, "\n")
-	vimPlugLines = append(vimPlugLines, string(v.Config))
-	vimPlugLines = append(vimPlugLines, "\n")
-	return strings.Join(vimPlugLines, "\n")
+	pluginLines = append(pluginLines, "}", "\n")
+	lines := append(pluginLines, string(v.Config))
+	return strings.Join(lines, "\n")
 }
 
 type Plugin struct {
@@ -164,7 +99,7 @@ type NvimConfig string
 
 func (c NvimConfig) MarshalJSON() ([]byte, error) {
 	if len(c) > 0 {
-		return json.Marshal(`"(present but omitted)"`)
+		return json.Marshal(`"(omitted for brevity)"`)
 	}
 	return []byte(`"(empty)"`), nil
 }
